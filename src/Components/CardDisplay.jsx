ad4bc3879436cdcd4c427ca2ce7507160a00204c
Irfanali958 <195564128+Irfanali958@users.noreply.github.com>
2026-04-19 19:38:14 +0530

    Update CardDisplay.jsx

--- src/Components/CardDisplay.jsx
+++ src/Components/CardDisplay.jsx
@@ -3,7 +3,7 @@ function CardDisplay({ poster, title, year, type }) {
     <>
       <div className="border border-2 rounded hover:scale-105 transition duration-300">
         src={poster}
-  alt={movie.Title}
+  alt={title}
   onError={(e) => {
     e.target.src = "https://via.placeholder.com/300x450?text=No+Image";
   }}
