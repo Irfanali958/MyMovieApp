f3024538dd8e24ae773af1b5b5abdd050b798fe3
Irfanali958 <195564128+Irfanali958@users.noreply.github.com>
2026-04-19 19:31:46 +0530

    Improve image error handling in CardDisplay
    Updated image handling in CardDisplay component.

--- src/Components/CardDisplay.jsx
+++ src/Components/CardDisplay.jsx
@@ -2,11 +2,11 @@ function CardDisplay({ poster, title, year, type }) {
   return (
     <>
       <div className="border border-2 rounded hover:scale-105 transition duration-300">
-        <img
-          src={poster !== "N/A" ? poster : "/placeholder.jpg"}
-          alt="Poster Image"
-          className="w-full rounded-t"
-        />
+        src={poster}
+  alt={movie.Title}
+  onError={(e) => {
+    e.target.src = "https://via.placeholder.com/300x450?text=No+Image";
+  }}
         <h2 className="font-bold">Title : {title}</h2>
         <p className="text-sm text-gray-500">Year : {year}</p>
         <p className="text-sm text-gray-500">Type : {type}</p>
