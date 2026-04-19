72182352c09cd769c375b0b477f9cdc3b7eec834
Irfanali958 <195564128+Irfanali958@users.noreply.github.com>
2026-04-19 19:39:55 +0530

    Fix image tag in CardDisplay component

--- src/Components/CardDisplay.jsx
+++ src/Components/CardDisplay.jsx
@@ -2,11 +2,12 @@ function CardDisplay({ poster, title, year, type }) {
   return (
     <>
       <div className="border border-2 rounded hover:scale-105 transition duration-300">
+        <img
         src={poster}
   alt={title}
   onError={(e) => {
     e.target.src = "https://via.placeholder.com/300x450?text=No+Image";
-  }}
+  }} />
         <h2 className="font-bold">Title : {title}</h2>
         <p className="text-sm text-gray-500">Year : {year}</p>
         <p className="text-sm text-gray-500">Type : {type}</p>
